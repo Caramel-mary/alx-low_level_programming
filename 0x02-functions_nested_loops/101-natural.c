5ae8e6f5d6597d8ed3d8683c5b153b3e1554e9a8
Caramel-mary <okachim5@gmail.com>
2023-04-25 17:14:49 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,12 +10,12 @@ int main(void)
 
 	while (i < 1024)
 	{
-	if ((i % 3 == 0) || (i % == 0))
+	if ((i % 3 == 0) || (i % 5 == 0))
 	{
 	z += i;
 	}
 	i++;
 	}
 	printf("%d\n", z);
-	return (0)
+	return (0);
 }
